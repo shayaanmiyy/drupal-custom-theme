06ae4966448004eddf127b5d18c5ab6d5a71d914
Shayaan <shayaan@digitalpolygon.com>
2024-09-30 00:52:59 +0530

    OS-70: Custom Image with Drupal Behaivour

--- web/themes/custom/mytheme/js/script.js
+++ web/themes/custom/mytheme/js/script.js
@@ -1,3 +1,16 @@
 window.onload = () => {
   // Write JS here
+  (function ($, Drupal) {
+    Drupal.behaviors.slickSlider = {
+      attach: function (context, settings) {
+        $('.slick-slider', context).once('slickSlider').slick({
+          autoplay: true,
+          dots: true,
+          infinite: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        });
+      }
+    };
+  })(jQuery, Drupal);
 }
