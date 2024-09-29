5625a0151f73a8a513a188cc582812b8a936531e
Shayaan <shayaan@digitalpolygon.com>
2024-09-30 01:11:11 +0530

    OS-71: Custom Message

--- web/themes/custom/mytheme/js/script.js
+++ web/themes/custom/mytheme/js/script.js
@@ -1,3 +1,26 @@
 window.onload = () => {
   // Write JS here
 }
+
+(function ($, Drupal, drupalSettings, once) {
+  Drupal.behaviors.welcomeMessage = {
+    attach: function (context, settings) {
+      if (drupalSettings.mytheme && drupalSettings.mytheme.welcomeMessage) {
+        var message = drupalSettings.mytheme.welcomeMessage;
+        once('welcomeMessage', 'body', context).forEach(function (element) {
+          var welcomeDiv = $('<div>', {
+            class: 'welcome-message',
+            text: message
+          }).prependTo(element);
+          welcomeDiv.css({
+            'background-color': '#f8f9fa',
+            'color': '#000',
+            'padding': '10px',
+            'text-align': 'center',
+            'font-size': '18px'
+          });
+        });
+      }
+    }
+  };
+})(jQuery, Drupal, drupalSettings, once);
