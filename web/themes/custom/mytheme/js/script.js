d608138531b6a75c558c4e9e505770238b6f78a8
Shayaan <shayaan@digitalpolygon.com>
2024-10-02 17:02:26 +0530

    task.OS-70: Custom Image Slick Slider in Drupal

--- web/themes/custom/mytheme/js/script.js
+++ web/themes/custom/mytheme/js/script.js
@@ -1,16 +1,23 @@
 window.onload = () => {
   // Write JS here
-  (function ($, Drupal) {
-    Drupal.behaviors.slickSlider = {
-      attach: function (context, settings) {
-        $('.slick-slider', context).once('slickSlider').slick({
+}
+
+(function ($, Drupal, once) {
+  Drupal.behaviors.slickSlider = {
+    attach: function (context, settings) {
+      const $slider = $('.slick-slider .field--name-field-slider .field__items');
+      once('slickSlider', $slider, context).forEach(function (element) {
+        $($slider).slick({
           autoplay: true,
           dots: true,
+          arrows: true,
+          prevArrow: '<button type="button" class="slick-prev">&#10094;</button>',
+          nextArrow: '<button type="button" class="slick-next">&#10095;</button>',
           infinite: true,
           slidesToShow: 1,
-          slidesToScroll: 1,
+          slidesToScroll: 1
         });
-      }
-    };
-  })(jQuery, Drupal);
-}
+      });
+    }
+  };
+})(jQuery, Drupal, once);
