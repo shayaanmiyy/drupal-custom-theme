eafc53e2c17be44d10e177be4c68b6ddce224ee5
Shayaan <shayaanmomin.sm@gmail.com>
2024-10-16 01:39:29 +0530

    OS-70: Slick SLider

--- web/themes/custom/mytheme/js/script.js
+++ web/themes/custom/mytheme/js/script.js
@@ -24,3 +24,23 @@ window.onload = () => {
     }
   };
 })(jQuery, Drupal, drupalSettings, once);
+
+(function ($, Drupal, once) {
+  Drupal.behaviors.slickSlider = {
+    attach: function (context, settings) {
+      const $slider = $('.slick-slider .field--name-field-slider .field__items');
+      once('slickSlider', $slider, context).forEach(function (element) {
+        $($slider).slick({
+          autoplay: true,
+          dots: true,
+          arrows: true,
+          prevArrow: '<button type="button" class="slick-prev">&#10094;</button>',
+          nextArrow: '<button type="button" class="slick-next">&#10095;</button>',
+          infinite: true,
+          slidesToShow: 1,
+          slidesToScroll: 1
+        });
+      });
+    }
+  };
+})(jQuery, Drupal, once);
